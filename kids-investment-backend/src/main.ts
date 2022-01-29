2e91e889812ecf1d93304b2346507d0729f32614
bangyuwen <bangyuwen@gmail.com>
2022-01-29 11:18:39 +0800

    init swagger

--- kids-investment-backend/src/main.ts
+++ kids-investment-backend/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { MembersModule } from './members/members.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(MembersModule);
-  app.useGlobalPipes(
-    new ValidationPipe()
-  );
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Members example')
+    .setDescription('The members API description')
+    .setVersion('1.0')
+    .addTag('members')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
