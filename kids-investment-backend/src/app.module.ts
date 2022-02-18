6704dc0c9c3b744d83638a52e95dd76ce249f1b5
Terence Liu <terenzio@gmail.com>
2022-02-18 20:34:11 +0800

    added ormconfig file to differentiate between dev and prod DB environment

--- kids-investment-backend/src/app.module.ts
+++ kids-investment-backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MembersModule } from './members/members.module';
 import { YoutubeModule } from './youtube/youtube.module';
@@ -8,15 +8,30 @@ import { Member } from './members/member.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [Member],
-      synchronize: true, //In PRODCUTION this should be false
-                         //to prevent overwriting the DB structure
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRoot(),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   port: parseInt(<string>'5432'),
+    //   username: 'postgres',
+    //   password: 'postgres',
+    //   database: 'kids_investment',
+    //   entities: [Member],
+    //   synchronize: true, //In PRODCUTION this should be false
+    //                      //to prevent overwriting the DB structure
   
-    }), 
+    // }), 
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [Member],
+    //   synchronize: true, //In PRODCUTION this should be false
+    //                      //to prevent overwriting the DB structure
+  
+    // }), 
     MembersModule,
     YoutubeModule,
     SocialsModule,
